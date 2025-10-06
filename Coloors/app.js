f8cae5e56b3acb3ce6f3ae4cf722a92fb185c90e
omotosho <toshieyadegoke@gmail.com>
2025-10-06 15:02:37 +0300

    updated the background color based on slider input

--- Coloors/app.js
+++ Coloors/app.js
@@ -7,6 +7,11 @@ const currentHexes = document.querySelectorAll(".color h2");
 // intial colors
 let intialColors;
 
+// Event Listeners
+sliders.forEach(slider => {
+    slider.addEventListener('input', hslControls);
+});
+
 // Functions
 function generateHex () {
     // const letters = "0123456789ABCDEF";
@@ -60,9 +65,44 @@ function colorizeSliders(color, hue, brightness, saturation) {
     const noSat = color.set('hsl.s',0);
     const fullSat = color.set('hsl.s', 1);
     const scaleSat = chroma.scale([noSat, color, fullSat]);
+    //Scale Brightness
+    const midBright = color.set("hsl.l", 0.5);
+    const scaleBright = chroma.scale(["black", midBright, "white"]);
 
     // update input colors
     saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(0)}, ${scaleSat(1)})`;
 
+    // brightness backgroundColor
+    brightness.style.backgroundImage = `linear-gradient(to right,${scaleBright(
+    0)},${scaleBright(0.5)} ,${scaleBright(1)})`;
+    
     // hue.style.backgroundColor
-}
+    hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
+};
+
+function hslControls (e) {
+    const index =
+        e.target.getAttribute ("data-bright") ||
+        e.target.getAttribute ("data-sat") ||
+        e.target.getAttribute ("data-hue");
+
+    console.log(index);
+
+    let sliders = e.target.parentElement.querySelectorAll('input[type="range"]');
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    console.log(sliders);
+
+    const bgColor = colorDivs[index].querySelector("h2").innerText;
+
+    let color = chroma(bgColor)
+        .set("hsl.s", saturation.value)
+        .set("hsl.l", brightness.value)
+        .set("hsl.h", hue.value);
+
+    colorDivs[index].style.backgroundColor = color;
+};
+// hslControls ();
+// console.log(colorDivs);
